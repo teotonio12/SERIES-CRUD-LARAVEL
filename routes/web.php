845be9ab030abafddb6269e7654e3d49f4c71f95
Renan Teotonio <renantn93@gmail.com>
2021-02-25 10:45:43 -0400

    IMPROVEMENTE: melhorado o sistema de rotas divididos em grupos por namespace e prefix

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,50 @@
 <?php
 
-use Illuminate\Auth\Events\Logout;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (){
-    return view('welcome');
-});
-
-Route::group(['namespace' => 'App\http\Controllers\\'], function () {
+Route::group(['namespace' => 'App\http\Controllers\\','prefix' => '/series'], function () {
    
-    Route::get('/series', 'SeriesController@index')->name('series.index');
+    Route::get('/', 'SeriesController@index')->name('series.index');
 
-    Route::get('/series/criar', 'SeriesController@create')->name('series.FormCreate')->middleware('autenticador');
+    Route::get('/criar', 'SeriesController@create')->name('series.FormCreate')->middleware('autenticador');
 
-    Route::post('/series/criar' , 'SeriesController@store')->name('series.store')->middleware('autenticador');
+    Route::post('/criar' , 'SeriesController@store')->name('series.store')->middleware('autenticador');
 
-    Route::post('/series/{id}/editaNome' , 'SeriesController@update')->name('series.update')->middleware('autenticador');
+    Route::post('/{id}/editaNome' , 'SeriesController@update')->name('series.update')->middleware('autenticador');
 
-    Route::delete('series/{id}' , 'SeriesController@destroy')->name('series.destroy')->middleware('autenticador');
+    Route::delete('/{id}' , 'SeriesController@destroy')->name('series.destroy')->middleware('autenticador');
 
-    Route::get('/series/{id}/temporadas', 'TemporadaController@index')->name('temporada.index');
-
-    Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index')->name('episodios.index');
-
-    Route::post('/temporadas/{temporada}/episodios/assitir' , 'EpisodiosController@assistir')->name('series.assistir')->middleware('autenticador');
-
-    Route::get('/entrar' , 'EntrarController@index')->name('entrar.index');
-
-    Route::post('/entrar' , 'EntrarController@entrar')->name('entrar.entrar');
-
-    Route::get('/registrar' , 'RegistroController@create')->name('registro.create');
-
-    Route::post('/registrar' , 'RegistroController@store')->name('registro.store');
-
-    Route::get('/sair', 'EntrarController@sair')->name('entrar.sair');
-       
-
-
-    
-
+    Route::get('/{id}/temporadas', 'TemporadaController@index')->name('temporada.index');
 
 });
 
 
+Route::group(['namespace' => 'App\http\Controllers\\','prefix' => '/temporadas'], function () {
+   
+    Route::get('/{temporada}/episodios', 'EpisodiosController@index')->name('episodios.index');
+
+    Route::post('/{temporada}/episodios/assitir' , 'EpisodiosController@assistir')->name('series.assistir')->middleware('autenticador');
+
+});
+
+Route::group(['namespace' => 'App\http\Controllers\\','prefix' => '/entrar'], function () {
+   
+    Route::get('/' , 'EntrarController@index')->name('entrar.index');
+
+    Route::post('/' , 'EntrarController@entrar')->name('entrar.entrar');
+
+});
+
+Route::group(['namespace' => 'App\http\Controllers\\','prefix' => '/registrar'], function () {
+   
+    Route::get('/' , 'RegistroController@create')->name('registro.create');
+
+    Route::post('/' , 'RegistroController@store')->name('registro.store');
+
+});
+
+Route::group(['namespace' => 'App\http\Controllers\\','prefix' => '/sair'], function () {
+   
+    Route::get('/', 'EntrarController@sair')->name('entrar.sair');
+
+});
